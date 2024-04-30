bb669b687085a69067def949e76e05f05170de2b
Van Dung Dev <vincentcode402112@gmail.com>
2024-04-30 22:17:19 +0700

    recursion

--- _Demo_Recursion.java
+++ _Demo_Recursion.java
@@ -4,8 +4,28 @@ public class _Demo_Recursion {
         printF();
     }
 
+    // Tinh n!, return n!
+    public static int GiaiThua(int n) {
+        // Bai toan co so
+        if(n == 0)
+            return 1;
+        // Cong thuc quy nap
+        return n * GiaiThua(n - 1);
+    }
+
+    // Tinh fibonaci thu n
+    public static int Fibo(int n) {
+        System.out.println("F[" + n + "]");
+        // Bai toan co so
+        if(n <= 2)
+            return 1;
+        // Cong thuc quy nap
+        int Fn = Fibo(n - 1) + Fibo(n - 2);
+        return Fn;
+    }
+
     public static void main(String[] args) {
-        printF();
+        System.out.println(Fibo(5));
     }
 
 }
